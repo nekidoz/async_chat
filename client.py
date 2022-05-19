8010cb5776af2cd320d97a19ad786ec71e9478ea
Никитос <Nikitos@MP-NV.local>
2022-05-19 21:57:30 +0300

    Lesson 4 objects

--- client.py
+++ client.py
@@ -4,28 +4,45 @@ import argparse
 import settings as sett
 import jim
 
-# Parse command-line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('address', nargs='?', default=None)
-parser.add_argument('port', nargs='?', default=None)
-args = parser.parse_args()
-server_address = args.address if args.address else sett.DEFAULT_SERVER_ADDRESS
-server_port = int(args.port) if args.port else sett.DEFAULT_PORT
-print(f"Connecting to {server_address if server_address else '(broadcast)'}:{server_port}")
 
-socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
-socket.connect((sett.DEFAULT_SERVER_ADDRESS, sett.DEFAULT_PORT))
-message = jim.Message(action=jim.Actions.PRESENCE, type="status",
-                      user={
-                          "account_name": "test",
-                          "status": "Online"
-                      }
-                      )
-socket.send(message.json.encode(sett.DEFAULT_ENCODING))
-response = jim.Response.from_str(socket.recv(sett.MAX_DATA_LEN).decode(sett.DEFAULT_ENCODING))
-print(f'Сообщение от сервера: ', response.json)
-if response.response == jim.Responses.OK:
-    print("Message acknowledged")
-else:
-    print(f"Unexpected return code: {response.response}")
-socket.close()
+# ATTRIBUTES:
+# address - server address
+# port - server port
+# socket - server socket
+class Client:
+    # initialize parameters and open server socket
+    def __init__(self, address: str = None, port: int = None):
+        # process parameters
+        self.address = address if address else sett.DEFAULT_SERVER_ADDRESS
+        self.port = port if port else sett.DEFAULT_PORT
+        print(f"Соединение с сервером по адресу {self.address}:{self.port}")
+        self.socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+        self.socket.connect((self.address, self.port))
+
+    def chat(self):
+        message = jim.Message(action=jim.Actions.PRESENCE, type="status",
+                              user={
+                                  "account_name": "test",
+                                  "status": "Online"
+                              }
+                              )
+        self.socket.send(message.json.encode(sett.DEFAULT_ENCODING))
+        response = jim.Response.from_str(self.socket.recv(sett.MAX_DATA_LEN).decode(sett.DEFAULT_ENCODING))
+        print(f'Сообщение от сервера: ', response.json)
+        if response.response == jim.Responses.OK:
+            print("Message acknowledged")
+        else:
+            print(f"Unexpected return code: {response.response}")
+        self.socket.close()
+
+
+if __name__ == "__main__":
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('address', nargs='?', default=None)
+    parser.add_argument('port', nargs='?', default=None)
+    args = parser.parse_args()
+    # Create a client and connect to the server
+    client = Client(args.address, args.port)
+    # Chat
+    client.chat()
--- server.py
+++ server.py
@@ -4,28 +4,54 @@ import argparse
 import settings as sett
 import jim
 
-# Parse command-line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('-address', required=False)
-parser.add_argument('-port', required=False)
-args = parser.parse_args()
-server_address = args.address if args.address else sett.DEFAULT_LISTEN_ADDRESS
-server_port = int(args.port) if args.port else sett.DEFAULT_PORT
-print(f"Listening on {server_address if server_address else '(all)'}:{server_port}")
+# ATTRIBUTES:
+# address - server address
+# port - server port
+# socket - server socket
+class Server:
+    # initialize parameters and open server socket
+    def __init__(self, address: str = None, port: int = None):
+        # process parameters
+        self.address = address if address else sett.DEFAULT_LISTEN_ADDRESS
+        self.port = port if port else sett.DEFAULT_PORT
+        print(f"Сервер ожидает соединений по адресу {self.address if self.address else '(все)'}:{self.port}")
+        # Create and bind a socket and listed to connections
+        self.socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+        self.socket.bind((self.address, self.port))
+        self.socket.listen()
 
-# Create and bind a socket and listed to connections
-socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
-socket.bind((server_address, server_port))
-socket.listen()
+    # accept and process client connections
+    def accept(self):
+        connection, address = self.socket.accept()
+        print(f"Установлено соединение с {address}.")
+        connection.settimeout(sett.CONNECTION_TIMEOUT)
+        try:
+            while True:
+                data = connection.recv(sett.MAX_DATA_LEN)
+                if not data:
+                    break
+                message = jim.Message.from_str(data.decode(sett.DEFAULT_ENCODING))
+                print('Сообщение: ', message.json)
+                if message.action == jim.Actions.PRESENCE:
+                    response = jim.Response(**jim.Responses.OK.response).json
+                else:
+                    response = jim.Response(**jim.Responses.BAD_REQUEST.response).json
+                connection.send(response.encode(sett.DEFAULT_ENCODING))
+        except TimeoutError:
+            print(f"Таймаут, соединение с {address} закрывается.")
+        else:
+            print(f"Соединение с {address} закрыто клиентом.")
+        connection.close()
 
-# Proccess client messages
-while True:
-    connection, address = socket.accept()
-    message = jim.Message.from_str(connection.recv(sett.MAX_DATA_LEN).decode(sett.DEFAULT_ENCODING))
-    print(f'Сообщение от {address}: ', message.json)
-    if message.action == jim.Actions.PRESENCE:
-        response = jim.Response(**jim.Responses.OK.response).json
-    else:
-        response = jim.Response(**jim.Responses.BAD_REQUEST.response).json
-    connection.send(response.encode(sett.DEFAULT_ENCODING))
-    connection.close()
+
+if __name__ == "__main__":
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-address', required=False)
+    parser.add_argument('-port', required=False)
+    args = parser.parse_args()
+    # Create a server and start listening
+    server = Server(args.address, args.port)
+    # Process client messages
+    while True:
+        server.accept()
